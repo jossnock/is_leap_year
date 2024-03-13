a6ce6837de317f969050a224028f71fe66f73263
jossnock <144459459+jossnock@users.noreply.github.com>
2024-03-13 15:40:55 +0000

    IT WORKS

--- is_leap_year.py
+++ is_leap_year.py
@@ -1,20 +1,25 @@
 def is_leap_year(year):
-    """Pass in a year (don't specift AD or BC)
+    """Pass in a year (don't specify AD or BC)
     If the year is a leap year, returns True
     If the year isn't a leap year, returns False"""
+    year = int(year)
     if year % 4 != 0:
         return False
-    if year % 100 == 0:
+    elif year % 100 == 0:
         if year % 400 == 0:
             return False
+        else:
+            return True
+    else:
         return True
-    return True
-
+    
+# asserts:
 assert(is_leap_year(2016)==True)
 assert(is_leap_year(2005)==False)
 assert(is_leap_year(400)==False)
 assert(is_leap_year(700)==True)
-    
-year = input("Input a year to see if it's a leap year (don't specift AD or BC):")
-print(is_leap_year(year))
 
+
+while True:
+    year = input("Input a year to see if it's a leap year (don't specift AD or BC):")
+    print(is_leap_year(year))
